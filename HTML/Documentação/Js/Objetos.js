3cf2f00de6c5219d47f1cf4b05c108f406b2ba7c
Felipe Franco <fe.franco97@gmail.com>
2023-11-13 17:42:05 -0300

    Novos items adicionados na doc de array

--- HTML/Documentação/Js/Objetos.js
+++ HTML/Documentação/Js/Objetos.js
@@ -1,27 +1,32 @@
-// Falando um pouco sobre objetos:
-
 let object = {
   nome: "Objeto",
   tipe: "Objeto",
 };
-
 object.tamo = "Junto";
-
 object["Sou"] = "uma propriedade";
-
 console.log(object);
-
 // Criação deu um objeto padrão
 function pessoa(nome, sobrenome) {
   return { nome: nome, sobrenome: sobrenome };
 }
-
 let person = pessoa("Fulano", "Beltrano"); // Para funções de criação de objeto não é preciso utliar NEW
-
+console.log(person);
 // Construtor de classe e Objeto
 function pessoa2(nome, sobrenome) {
   this.nome = nome;
   this.sobrenome = sobrenome;
 }
-
 let person2 = new pessoa2("Fulano", "Beltrano"); // Para construtores é necessário sempre utilizar o NEW
+console.log(person2);
+
+// Métodos importantes de Objetos:
+person.hasOwnProperty("nome"); // Valida se a propriedade 'nome' existe
+// Exemplo de uso
+const validaPropriedade = (obj, prop) => obj.hasOwnProperty(prop); // Essa função retorna true ou false, caso a propriedade exista dentro do objeto
+console.log(validaPropriedade());
+
+const valores = Object.values(person); // Retorna somente os valores das propriedades de um objeto em forma de array, sendo possivel realizar a soma de todos os valores EX:
+console.log(valores.reduce((v1, v2) => v1 + v2));
+
+const chaves = Object.keys(person); // Retorna somente os valores das chaves sem as propriedades de um objeto em forma de array
+console.log(chaves);
