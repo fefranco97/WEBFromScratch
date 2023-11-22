6cd91d507514b1a902815eabf39221c5f332e345
Felipe Franco <fe.franco97@gmail.com>
2023-11-22 10:52:04 -0300

    Melhoria no churrascometro

--- HTML/2_Intermediario/Projeto_4_ProgamadorBR/script.js
+++ HTML/2_Intermediario/Projeto_4_ProgamadorBR/script.js
@@ -1,24 +1,5 @@
 let qntAdultos, qntCriancas, qntHoras;
 
-document.body.innerHTML = `<form action="" method="">
-        <h2>Churrascômetro</h2>
-        <label for="adulto"> Adulto
-            <input id="adulto" type="number" placeholder="Insira a quantidade de adultos" min="0" value="0"
-                onchange="valueHandler(value, 'adulto')">
-        </label>
-        <label for="crianca">Criança
-            <input id="crianca" type="number" placeholder="Insira a quantidade de adultos" min="0" value="0"
-                onchange="valueHandler(value, 'crianca')">
-        </label>
-        <label for="horas"> Horas
-            <input id="horas" type="number" placeholder="Insira a quantidade de Horas" min="0" value="0"
-                onchange="valueHandler(value, 'horas')">
-        </label>
-        <label for="calcular">
-            <input id="calcular" onclick="onCalculateButton()" class="inputBtn" type="button" value="Calcular">
-        </label>
-    </form>`;
-
 function valueHandler(value, type) {
   switch (type) {
     case "adulto":
@@ -45,29 +26,45 @@ function calculateValue() {
       ((qntAdultos * 200) / 1000 + (qntCriancas * 105) / 1000) *
       qntHoras
     ).toFixed(2),
-    totalBebidas: qntAdultos * 4 * qntHoras,
+    totalBebidas: qntAdultos * 3 * qntHoras,
   };
 }
 
 function onCalculateButton() {
   const result = calculateValue();
-  renderHTML(result);
+
+  document.getElementById(
+    "carne"
+  ).innerText = `Total de carne: ${result.totalCarne} Kg`;
+  document.getElementById(
+    "bebida"
+  ).innerText = `Total de latinhas de cerveja: ${result.totalBebidas} Latinhas`;
 }
 
-function renderHTML(valores) {
-  const div = document.createElement("div");
-  div.classList.add("totalContainer");
-  const p1 = document.createElement("p");
-  const p2 = document.createElement("p");
-  const carne = document.createTextNode(
-    `Total de carne: ${valores.totalCarne} Kg`
-  );
-  const bebidas = document.createTextNode(
-    `Total de latinhas de cerveja: ${valores.totalBebidas} Latinhas`
-  );
-  p1.appendChild(carne);
-  p2.appendChild(bebidas);
-  div.appendChild(p1);
-  div.appendChild(p2);
-  document.body.appendChild(div);
+function renderHTML() {
+  document.body.innerHTML = `<form action="" method="">
+        <h2>Churrascômetro</h2>
+        <label for="adulto"> Adulto
+            <input id="adulto" type="number" placeholder="Insira a quantidade de adultos" min="0" value="0"
+                onchange="valueHandler(value, 'adulto')">
+        </label>
+        <label for="crianca">Criança
+            <input id="crianca" type="number" placeholder="Insira a quantidade de adultos" min="0" value="0"
+                onchange="valueHandler(value, 'crianca')">
+        </label>
+        <label for="horas"> Horas
+            <input id="horas" type="number" placeholder="Insira a quantidade de Horas" min="0" value="0"
+                onchange="valueHandler(value, 'horas')">
+        </label>
+        <label for="calcular">
+            <input id="calcular" onclick="onCalculateButton()" class="inputBtn" type="button" value="Calcular">
+        </label>
+    </form>
+    <div class="totalContainer">
+        <p id="carne"></p>
+        <p id="bebida"></p>
+    </div>
+    `;
 }
+
+renderHTML();
