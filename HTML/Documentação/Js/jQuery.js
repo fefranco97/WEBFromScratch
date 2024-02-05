05743971ef224d23d2690050f04cbf6fef28c04d
FeFranco <fe.franco97@gmail.com>
2024-02-05 12:25:36 -0300

    :books:[Docs]:Melhoria na documentação do Jquery

--- HTML/Documentação/Js/jQuery.js
+++ HTML/Documentação/Js/jQuery.js
@@ -34,10 +34,59 @@ $(() => {
     "<li>Novo item 1</li><li>Novo item 2</li><li>Novo item 3</li>"
   );
 
+  const lista4 = $("#lista4");
+
   // ALtera o atributo nome dos inputs para o valor + um index
   $("#lista4")
     .children("li")
+    .attr("key", (index) => {
+      return index;
+    })
     .attr("name", (index, oValue) => {
       return oValue + index;
     });
+
+  // Remove todos o items filhos o elemento selecionado, por exemplo remove todos os "li" do lista 4
+  // $("#lista4").empty();
+
+  lista4.append("<li class='item'>Append</li>"); // Adiciona um item no final do bloco do elemento
+  lista4.prepend("<li class='item'>prepend</li>"); // Adiciona um item no inicio do bloco do elemento
+
+  lista4.after("<p>After</p>"); // Adiciona um itm após o bloco do elemento
+  lista4.before("<p>before</p>"); // Adiciona um itm antes o bloco do elemento
+
+  // CSS com Jquery
+  function clickHandler() {
+    const attribute = $(this).attr("key");
+    setColorDiv(attribute);
+  }
+
+  function setColorDiv(id) {
+    switch (id) {
+      case "btn1":
+        $("[key = div1]").addClass("bg-primary");
+        break;
+      case "btn2":
+        $("[key = div2]").removeClass("bg-primary");
+        break;
+      case "btn3":
+        $("[key = div3]").toggleClass("bg-primary");
+        break;
+      case "btn4":
+        $("[key = div4]").toggleClass("bg-secondary");
+        break;
+      case "btn5":
+        $("[key = div5]").toggleClass("bg-contrast ");
+        break;
+      default:
+        break;
+    }
+  }
+
+  const allButtonsOnDiv = $(".cssBtn");
+
+  for (button of allButtonsOnDiv) {
+    let key = button.attributes["key"].value;
+    $(`[key = ${key}]`).on("click", clickHandler);
+  }
 });
